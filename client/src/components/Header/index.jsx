20f674de50ceccf8ed858c527b9c74d9d03b5535
Nate Foster <soundwubz@gmail.com>
2020-05-23 14:02:51 -0400

    Logout working

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,27 +1,42 @@
 import React from "react";
 import "./style.css";
 import { getFromStorage, verifyToken } from "../../utils/storage";
+import { Redirect } from "react-router-dom";
 
 class Header extends React.Component {
 
+    state = {
+        navigate: false
+    }
+
     logout = () => {
         const obj = getFromStorage('nort');
-        verifyToken('/api/logout?token=', obj);
+        const loggedOut = verifyToken('/api/logout?token=', obj);
+        if(loggedOut) {
+            this.setState({navigate: true});
+        }
     }
     render() {
-        return (
-            <div className="container-fluid header">
-                <div className="row">
-                    <div className="col"></div>
-                    <div className="col align-center">
-                        <h1>N.O.R.T</h1>
-                    </div>
-                    <div className="col">
-                        <button className="btn" onClick={this.logout}>Logout</button>
+        if(this.state.navigate === true) {
+            this.setState({navigate: false});
+            return (
+                <Redirect to={'/login'} />
+            )
+        } else {
+            return (
+                <div className="container-fluid header">
+                    <div className="row">
+                        <div className="col"></div>
+                        <div className="col">
+                            <h1>N.O.R.T</h1>
+                        </div>
+                        <div className="col text-center align-middle">
+                            <button className="btn logout" onClick={this.logout}>Logout</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
 }
 
