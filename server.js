340a6264b05cb02c185b837602de7c56232ec333
Nate Foster <soundwubz@gmail.com>
2020-05-29 10:40:28 -0400

    production update

--- server.js
+++ server.js
@@ -25,12 +25,15 @@ app.use(session({ secret: "tron", resave: false, saveUninitialized: false, maxAg
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.urlencoded({ extended: false }));
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
 // Add routes, both API and view
 app.use(routes);
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  })
+}
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nort", { useFindAndModify: false });
 
